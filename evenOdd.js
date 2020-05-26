3caed5b9a6542bf02815159cb23b840c297192e3
Kasia Janiszewska <salt-dev@MacBook-Pro.local>
2020-05-26 16:11:58 +0200

    changes to the evenOdd

--- evenOdd.js
+++ evenOdd.js
@@ -1,13 +1,21 @@
 function keep(arr){
     arr.map(el => {
-        el%2 === 0 ? console.log(el) : false;
+        el%2 === 0 ? console.log("Even number is: " + el) : false;
     })
 }
 
 function discard(arr){
     arr.map(el => {
-        el%2 !== 0 ? console.log(el) : false;
+        el%2 !== 0 ? console.log("Odd number is: " + el) : false;
     })
+}
+
+function accumulate(arr){
+    let results = [];
+    arr.map(el => {
+        results.push(el*el);
+    })
+    console.log(results)
 }
 
 function check(callback, arr){
@@ -16,3 +24,4 @@ function check(callback, arr){
 
 check(keep, [1,2,3,4,5]);
 check(discard, [1,2,3,4,5]);
+check(accumulate, [1,2,3,4,5]);
